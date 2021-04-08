7dc977eb062180ca0c011a08c7b9d2925ad6de99
all persons <35461002238898@mail.ru>
2021-04-08 04:58:52 +0300

    add new code

--- attachment/js/replaceKeys.js
+++ attachment/js/replaceKeys.js
@@ -10,20 +10,25 @@ let obj = [
 // console.log(obj.forEach(item => item.name)); // не работает...
 
 // for (let key in obj) {
-//     console.log(obj[key])
-//     console.log(obj[key].name)
+    // console.log(obj[key].name)
+    // console.log(obj[key])
 // }
 
 // console.log(Object.entries(obj))
 // console.log(Object.values(obj))
 
-for (let value of obj) {
-    console.log(value.age)
+// for (let value of obj) {
+//     console.log(value)
+
+// }
+
+
+function convertObject(obj) {
+    return obj.reduce((acc, item) => {
+        acc += item
+    }, [])
 }
 
-
-// function convertObject(obj) {
-//     let 
-// }
+console.log(convertObject(obj))
 
 // convertObject({ a: 'b', c: 'd' }) // => { b: 'a', d: 'c' }
